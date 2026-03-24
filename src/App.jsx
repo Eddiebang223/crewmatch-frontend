e64f9c357c51e283812d41aa444a89972c0078d3
Eddiebang223 <eddiebangari74@gmail.com>
2026-03-24 04:37:17 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🚀 CrewMatch</h1>
+      <p>Your app is ready!</p>
+    </div>
+  );
+}
+
+export default App;
